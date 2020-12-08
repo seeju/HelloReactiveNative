e41d97cee85f3e5b3352b2b5e4baa13523b938ec
seeju <julianap.fer@gmail.com>
2020-12-07 22:59:29 -0300

    ex 06

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, StyleSheet, FlatList } from "react-native";
+import { Reddit } from "./api/Reddit";
+import { RedditItem } from "./components/RedditItem";
+const App = () => {
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    const fetchReddit = async () => {
+      const results = await Reddit.get("javascript");
+      setItems(results);
+    };
+    fetchReddit();
+  }, []);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <RedditItem item={item} />}
+      ></FlatList>
+    </SafeAreaView>
   );
-}
-
+};
+export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff"
+  }
 });
